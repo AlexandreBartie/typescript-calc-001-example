e407919f0cbe6069cbdd0a2718105eb36e78fcb0
Alexandre Bartie <bartie.devops@outlook.com>
2024-04-15 00:37:38 -0300

    renamed project to avoid 24 hours blocked to publish

---
+++ index.ts
@@ -0,0 +1,4 @@
+import { Calc } from './src/classes/calc';
+import { AnotherClass } from './src/classes/other';
+
+export { Calc, AnotherClass };
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import { Calc } from './classes/calc';
-import { AnotherClass } from './classes/other';
-
-export { Calc, AnotherClass };
